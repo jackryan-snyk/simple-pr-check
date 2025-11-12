d18160161063e2d88d477c0bcf5fd24bac805e95
Jack <jack.ryan@snyk.io>
2025-11-12 16:33:10 -0500

    Cross-site Scripting (XSS)

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static('public'));
 
+app.post('/upload', upload.single('pdf'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  res.send(`File uploaded: ${req.file.filename}`);
+});
 
 
 
